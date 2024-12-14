0f457cb9a446887d444db1243ccadc0db62c8be6
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-12-13 21:33:21 -0800

    Remove dead code

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,30 +1,6 @@
 export const MAX_ROWS = 47;
 export const MAX_COLS = 50;
 
-export const START_TILE_CONFIGURATION = {
-  row: 1,
-  col: 1,
-  isEnd: false,
-  isWall: false,
-  isPath: false,
-  distance: 0,
-  isStart: false,
-  isTraversed: false,
-  parent: null,
-};
-
-export const END_TILE_CONFIGURATION = {
-  row: MAX_ROWS - 2,
-  col: MAX_COLS - 2,
-  isEnd: false,
-  isWall: false,
-  isPath: false,
-  distance: 0,
-  isStart: false,
-  isTraversed: false,
-  parent: null,
-};
-
 export const TILE_STYLE =
   "lg:w-[20px] md:w-[15px] w-[12px] lg:h-[20px] md:h-[15px] h-[12px] border-t border-r border-sky-200";
 export const TRAVERSED_TILE_STYLE = TILE_STYLE + " bg-cyan-600";
